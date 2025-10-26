3129550127db9649b89da2e994ee7db71ba5e955
Lazar Savkovic <savkoviclazar1@gmail.com>
2025-10-26 19:20:00 +0100

    newest phone 6

--- app.js
+++ app.js
@@ -199,7 +199,8 @@ const c = canvas.getContext('2d')
 
 const MOVE_SPEED = canvasWidth / 300;
 const GRAVITY = canvasHeight / 1080;
-const JUMP_SPEED = GRAVITY * 25;
+const isMobile = window.innerWidth < 768;
+const JUMP_SPEED = isMobile ? GRAVITY * 35 : GRAVITY * 25;
 const startingPosition = canvasWidth * 1/6;
 let scroll = 0;
 
